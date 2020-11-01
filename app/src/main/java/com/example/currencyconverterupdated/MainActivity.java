81bae05ff6629d8cc6baeb9c49af4ca334afab8a
Parzival <tran.long1772000@gmail.com>
2020-11-01 23:03:27 +0700

    upgraded commit

--- app/src/main/java/com/example/currencyconverterupdated/MainActivity.java
+++ app/src/main/java/com/example/currencyconverterupdated/MainActivity.java
@@ -15,12 +15,12 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
     List<String> listCurrency;
     TextView textView;
     EditText editText;
-    double amount, result;
-    int pos1, pos2;
+    double amount = 0, result = 0 ;
+    int pos1 = 0, pos2 = 0;
     Spinner spinner1, spinner2;
     //double usa2vnd = 23.195, usa2cup = 26.5, usa2hrk = 6.40486, usa2jpy = 104.509, usa2rub = 77.1351, usa2kpw = 899.976, usa2cny = 6.70570, usa2krw = 1128.18;
     double usaChange2[] = {23.195, 1, 26.5, 6.40486, 104.509, 77.1351, 899.976, 6.70570, 1128.18};
@@ -38,13 +38,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         listCurrency.add("North Korea (KPW)");
         listCurrency.add("China (CNY)");
         listCurrency.add("Korea (KRW)");
-
-        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this,
-                R.layout.support_simple_spinner_dropdown_item,
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_dropdown_item,
                 listCurrency);
         spinner1 = findViewById(R.id.spinner1);
-        spinner1.setAdapter(adapter1);
-        spinner1.setSelection(1);
+        spinner1.setAdapter(adapter);
         spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -53,51 +51,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-
             }
         });
-        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this,
-                R.layout.support_simple_spinner_dropdown_item,
-                listCurrency);
+
         spinner2 = findViewById(R.id.spinner2);
-        spinner2.setAdapter(adapter2);
-        spinner2.setSelection(1);
+        spinner2.setAdapter(adapter);
         spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 pos2 = position;
             }
-
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
 
             }
         });
-        findViewById(R.id.btn_reset).setOnClickListener(this);
-        findViewById(R.id.btn_convert).setOnClickListener(this);
-        editText.findViewById(R.id.edit_amount);
-        textView = findViewById(R.id.tv_result);
-    }
 
-    @Override
-    public void onClick(View v) {
-        int id = v.getId();
-        if(id == R.id.btn_convert){
-            if(spinner1.hasFocus() && spinner2.hasFocus() && editText.hasFocus()){
+
+        findViewById(R.id.btn_reset).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("");
+                editText.setText("");
+            }
+        });
+        findViewById(R.id.btn_convert).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 amount = Double.parseDouble(editText.getText().toString());
                 result = convertCurrency(amount, pos1, pos2);
-                textView.setText(String.valueOf(result));
+                textView.setText(String.valueOf((double) Math.round(result*1000)/1000));
+
             }
-        }
-        if(id == R.id.btn_reset){
-            spinner1.clearFocus();
-            spinner2.clearFocus();
-            editText.setText("");
-            textView.setText("");
-        }
+        });
+        editText = findViewById(R.id.edit_amount);
+        textView = findViewById(R.id.tv_result);
     }
 
     private double convertCurrency(double amount, int from, int to){
         return amount/usaChange2[from] * usaChange2[to];
     }
+
+
 }
